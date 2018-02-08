775bd5206dc3fa72cc03d0c99a27df1c0f91ff94
aikuakdi <1774104802@qq.com>
2018-02-08 18:34:19 +0800

    improve by left-join as last

--- sql/config_user_click_weight_parameter.sql
+++ sql/config_user_click_weight_parameter.sql
@@ -3,13 +3,10 @@ overwrite into table config_user_click_weight_parameter;
 
 drop table config_user_click_weight_parameter;
 CREATE TABLE config_user_click_weight_parameter
-(keyword STRING COMMENT '关键词/字',
+(
+keyword STRING COMMENT '关键词/字',
 weight   FLOAT COMMENT '权重系数'
 )
 comment "V_nxk*Sigma_kxk.I*weight"
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' 
 STORED AS TEXTFILE;
-
-SELECT *
-from config_user_click_weight_parameter
-limit 10;
